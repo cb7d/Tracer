bfbbd90962aea792f070828c76c77ba790261492
Felix <zyhzhangyunhao@gmail.com>
2019-08-16 19:41:29 +0800

    refactor: parser

--- Sources/Tracer/Tracer/Tracer.swift
+++ Sources/Tracer/Tracer/Tracer.swift
@@ -15,112 +15,52 @@ class Tracer {
     
     let filePath: Path
     
-    fileprivate let semaphore = DispatchSemaphore(value: maxConcurrent)
-    
     required init(_ path: String) {
         filePath = Path(path)
     }
     
     func run() {
         
+        var invokers = [String]()
+        var classes = [String]()
         
-        
-        //        var classes = Set<String>()
-        //        var invokers = Set<String>()
+        func getFinalInvoker(_ invoker: ObjCInvoker) -> String {
+            
+            switch invoker {
+            case .variable(let name):
+                return name
+            case .otherInvoke(let otherInvoke):
+                return getFinalInvoker(otherInvoke.invoker)
+            }
+        }
         
         let paths = filePath.files().filter{ $0.fileIsObjC() }.map{ $0.stringValue() }
         paths.forEach{ p in
             
-            //            semaphore.wait()
-            //
-            //            DispatchQueue.global().async {
-            //
-            //
-            //
-            //                self.semaphore.signal()
-            //            }
-            
-            
             let tokens = LLexer(p).tokens
             
-//            if let nodes = parser_OCInterface.run(tokens) {
-//
-//                print("Interfaces")
-//                print(nodes)
-//
+//            if let ocimplements = parser_OCImplement.repeats.run(tokens) {
+//                ocimplements.forEach({ (impl) in
+//                    classes.append(impl.name)
+//                    impl.methods.forEach({ (method) in
+//                        method.invokes.forEach({(invoke) in
+//                            let invoker = invoke.invoker
+//                            invokers.append(getFinalInvoker(invoker))
+//                        })
+//                    })
+//                })
 //            }
-//
-//            if let nodes = parser_OCImplement.run(tokens) {
-//
-//                print("Implement")
-//                print(nodes)
-//
-//            }
-            
-            if let nodes = parser_OCMethodDefine.repeats.run(tokens) {
+            if let interfaces = parser_OCInterface.run(tokens) {
                 
-                print("Methods")
-                print(nodes)
                 
             }
             
-            
-            
-            
-            //            return
-            
-            //            if let interfaces = ObjCInterfaceConsumer.run(tokens) {
-            //                interfaces.forEach{
-            //                    classes.insert($0.className)
-            //                    //                        if let superClassName = $0.superClass {
-            //                    //                            classes.insert(superClassName)
-            //                    //                        }
-            //                }
-            //            }
-            
-            
-            
-            //            if let tks = ObjCFuncDefineConsumer.run(tokens) {
-            //
-            //
-            //                tks.forEach{funcNode in
-            //                    funcNode.invokes.forEach{invoke in
-            //
-            //                        var wait = true
-            //                        var invoker = invoke.invoker
-            //                        while wait {
-            //                            switch invoker {
-            //                            case .variable(let name):
-            //                                invokers.insert(name)
-            //                                wait = false
-            //                            case .invokeNode(let node):
-            //                                invoker = node.invoker
-            //                            }
-            //                        }
-            //                    }
-            //                }
-            //            }
-            
-            
         }
-        
-        //        waitUntilFinished()
-        
-        //        let uselessClass = classes.filter{!invokers.contains($0)}
-        //        print(uselessClass.sorted())
-        
-        
-    }
-}
-
-extension Tracer {
-    
-    private func waitUntilFinished() {
-        for _ in 0..<maxConcurrent {
-            semaphore.wait()
-        }
-        for _ in 0..<maxConcurrent {
-            semaphore.signal()
-        }
+        print("============")
+        print(invokers)
+        print("============")
+        print(classes)
+        print("============")
+        print(classes.filter{!invokers.contains($0)}.filter{!$0.hasSuffix("Cell")})
     }
 }
--- Sources/Tracer/main.swift
+++ Sources/Tracer/main.swift
@@ -2,9 +2,12 @@
 
 
 
+//let path = "/Users/felix/Documents/GitLab/TDFMallShop/TDFMallShop"
+//let path = "/Users/felix/Documents/GitLab/TDFEmployeeModule/TDFEmployeeModule"
+//let path = "/Users/felix/Documents/GitLab/TDFHomeModule/TDFHomeModule"
 
-
-let path = "/Users/felix/Documents/GitLab/TDFNetworking/TDFNetworking"
+let path = "/Users/felix/Documents/Test/tracerTest"
+//let path = "/Users/felix/Documents/GitLab/TDFNetworking/TDFNetworking"
 //let path = "/Users/felix/Documents/GitLab/TDFTestProject/Example/TDFTestProject"
 //let path = "/Users/felixplus/Documents/GitHub/AFNetworking"
 //let path = "/Users/felix/Documents/GitLab/TDFLogin"
